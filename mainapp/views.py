73bb2e8489c80ec2b5e1240d2443c3fb1dc51be1
Alexey <happyalex777@gmail.com>
2023-05-14 18:36:06 +0300

    change templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 
@@ -16,8 +16,12 @@ class DocSiteView(TemplateView):
     template_name = 'doc_site.html'
 
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+# class IndexView(TemplateView):
+#     template_name = 'index.html'
+
+
+def index(requset):
+    return render(requset, 'index.html')
 
 
 class LoginView(TemplateView):
@@ -27,3 +31,9 @@ class LoginView(TemplateView):
 class NewsView(TemplateView):
     template_name = 'news.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = 'Новость раз'
+        context_data['preview'] = 'Превью к новости раз'
+        context_data['date'] = '2022-01-01'
+        return context_data
